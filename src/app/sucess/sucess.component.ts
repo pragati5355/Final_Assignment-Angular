b993285566f04e738b66cf04e1068dc9c7e7e243
Your Name <you@example.com>
2022-08-01 17:40:52 +0530

    added the swal for sucessfull purchase

--- src/app/sucess/sucess.component.ts
+++ src/app/sucess/sucess.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  Swal  from 'sweetalert2';
 
 @Component({
   selector: 'app-sucess',
@@ -10,6 +11,18 @@ export class SucessComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    Swal.fire({
+      title: 'Purchase Successful',
+      html: '<div><p style="font-family:"Poppins";font-style: normal;font-weight: 300;font-size: 18px;line-height: 30px;">You will get your product soon! <br /><br/>to experience the spatial audio withadaptive EQ that tunes music to your ears.</p></div>',
+      imageUrl: 'assets/_images/purchaseimg.png',
+      imageWidth: 100,
+      imageHeight: 100,
+      imageAlt: 'Custom image',
+    })
+    
   }
+
+  
 
 }
